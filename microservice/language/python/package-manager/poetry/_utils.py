06fb01710f641a02a4ebfab094c1247027fca16a
Sergio García Prado <sergio.garcia@clariteia.com>
2022-01-13 13:35:58 +0100

    ISSUE #12 * Migrate microservice templates from `makefile` to `ansible`.

---
+++ microservice/language/python/package-manager/poetry/_utils.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import yaml
+
+
+def build_deploy_playbook(path: Path) -> str:
+    """Build Deploy Playbook file content."""
+    data = None
+    if path.exists():
+        with path.open() as file:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+
+    if data is None:
+        data = list()
+
+    data.append({
+        "name": "Lock Dependencies",
+        "import_playbook": "lock.yaml"
+    })
+
+    return yaml.dump(data, sort_keys=False)
