06d58a9eb33493348ec5daebfea91e36e3338684
albamig <albamigh@gmail.com>
2022-03-22 09:37:28 +0100

    - Added init folder.
    issue #52

--- microservice/language/python/init/_utils.py
+++ microservice/language/python/init/_utils.py
@@ -28,9 +28,9 @@ def build_deploy_playbook(path: Path) -> str:
 def build_docker_compose(path: Path, microservice_name: str) -> str:
     """Build Docker Compose file content."""
 
-    db_creation_path = path.parent / "external/postgres/10-create-database.sql"
+    db_creation_path = path.parent / "external/init/postgres/10-create-database.sql"
     if not db_creation_path.exists():
-        raise ValueError("external/postgres/10-create-database.sql script must exist")
+        raise ValueError("external/postgres/init/10-create-database.sql script must exist")
 
     with db_creation_path.open("a") as db_creation_file:
         db_creation_file.write(f"\nCREATE DATABASE {microservice_name}_db;")
